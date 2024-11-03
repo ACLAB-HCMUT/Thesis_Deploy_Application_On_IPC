13d6c1d6b20a3f96b02d0d38887dcfe28b893127
Phuc <phucquangle0503@gmail.com>
2024-11-03 09:18:37 +0700

    update login page

--- views/signin.py
+++ views/signin.py
@@ -1,33 +1,50 @@
 import customtkinter as ctk
+from PIL import Image
 
 class SignInView(ctk.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # Set up image
+        self.side_img_data = Image.open("assets/img/side_img.png")
+        self.email_icon_data = Image.open("assets/img/email_icon.png")
+        self.password_icon_data = Image.open("assets/img/password_icon.png")
+        self.google_icon_data = Image.open("assets/img/google_icon.png")
+
+        self.side_img = ctk.CTkImage(dark_image=self.side_img_data, light_image=self.side_img_data, size=(500, 600))
+        self.email_icon = ctk.CTkImage(dark_image=self.email_icon_data, light_image=self.email_icon_data, size=(20, 20))
+        self.password_icon = ctk.CTkImage(dark_image=self.password_icon_data, light_image=self.password_icon_data, size=(20, 20))
+        self.google_icon = ctk.CTkImage(dark_image=self.google_icon_data, light_image=self.google_icon_data, size=(17, 17))
+
         # Configure grid layout
         self.grid_columnconfigure(0, weight=0)
         self.grid_columnconfigure(1, weight=1)
 
-        # Create login frame with rounded corners and a background color
-        self.login_frame = ctk.CTkFrame(self, corner_radius=20, fg_color="#232f34")
-        self.login_frame.place(relx=0.5, rely=0.5, anchor="center")
+        # Create side image
+        ctk.CTkLabel(self, text="", image=self.side_img).pack(expand=True, side="left")
+
+        # Create login frame
+        self.login_frame = ctk.CTkFrame(self, width=310, height=600, fg_color="#FFFFFF")
+        self.login_frame.pack_propagate(0)
+        self.login_frame.pack(expand=True, side="right")
 
         # Login label
-        self.login_label = ctk.CTkLabel(self.login_frame, text="Login Page", font=ctk.CTkFont(size=30, weight="bold"), text_color="#FFFFFF")
-        self.login_label.grid(row=0, column=0, padx=35, pady=(20,20))
+        self.login_label = ctk.CTkLabel(self.login_frame, text="Login Page", font=ctk.CTkFont(size=30, weight="bold"), text_color="#601E88", anchor="w", justify="left").pack(anchor="w", pady=(50, 5), padx=(25, 0))
 
         # Email entry
-        self.email_entry = ctk.CTkEntry(self.login_frame, width=200, placeholder_text="Email")
-        self.email_entry.grid(row=1, column=0, padx=35, pady=(20, 20))
+        self.email_label = ctk.CTkLabel(self.login_frame,text="  Email:", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14), image=self.email_icon, compound="left").pack(anchor="w", pady=(38, 0), padx=(25, 0))
+        self.email_entry = ctk.CTkEntry(self.login_frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=1, text_color="#000000")
+        self.email_entry.pack(anchor="w", pady=(0, 20), padx=(25, 0))
 
         # Password entry
-        self.password_entry = ctk.CTkEntry(self.login_frame, width=200, show="*", placeholder_text="Password")
-        self.password_entry.grid(row=2, column=0, padx=35, pady=(0, 20))
+        self.email_label = ctk.CTkLabel(self.login_frame,text="  Password:", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14), image=self.password_icon, compound="left").pack(anchor="w", padx=(25, 0))
+        self.password_entry = ctk.CTkEntry(self.login_frame, width=225, show="*", fg_color="#EEEEEE", border_color="#601E88", border_width=1, text_color="#000000")
+        self.password_entry.pack(anchor="w", padx=(25, 0))
 
         # Login button
-        self.login_btn = ctk.CTkButton(self.login_frame, text="Login", width=200, fg_color="#F9AA33", hover_color="#E6952D")
-        self.login_btn.grid(row=3, column=0, padx=60, pady=(0, 30))
+        self.login_btn = ctk.CTkButton(self.login_frame, text="Login", width=200, height=50, fg_color="#601E88", hover_color="#E6952D", text_color="#FFFFFF", font=("Arial Bold", 18))
+        self.login_btn.pack(anchor="w", pady=(40, 0), padx=(35, 0))
 
         # Sign Up button
-        self.signup_btn = ctk.CTkButton(self.login_frame, text="Sign up", width=200, fg_color="#F9AA33", hover_color="#E6952D")
-        self.signup_btn.grid(row=4, column=0, padx=60, pady=(0, 30))
+        self.signup_btn = ctk.CTkButton(self.login_frame, text="Sign up", width=150, height=50, fg_color="#eeeee4", hover_color="#E6952D", text_color="#601E88", font=("Arial Bold", 18))
+        self.signup_btn.pack(anchor="w", pady=(20, 0), padx=(60, 0))
